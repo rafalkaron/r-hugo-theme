f4b1ddc3e4908ff0f9db70cb041fbd0521a55c22
rafalkaron <rafalkaron@gmail.com>
2022-06-03 00:18:36 +0200

    Add offset

--- themes/r-hugo-theme/static/js/hide-navbar.js
+++ themes/r-hugo-theme/static/js/hide-navbar.js
@@ -1,7 +1,7 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos || window.pageYOffset < 50) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-4rem";
