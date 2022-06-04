aa8acab063d1d8b1e11b45f60c83c7a7d261d277
rafalkaron <rafalkaron@gmail.com>
2022-06-05 00:02:01 +0200

    update

--- themes/r-hugo-theme/static/js/hide-navbar.js
+++ themes/r-hugo-theme/static/js/hide-navbar.js
@@ -1,11 +1,13 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos || window.pageYOffset < 30) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-4rem";
-    document.getElementById("menu-toggle").checked = false;
+  if (document.documentElement.clientWidth < 768) {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos || window.pageYOffset < 30) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-4rem";
+      document.getElementById("menu-toggle").checked = false;
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
 };
