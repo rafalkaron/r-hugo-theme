2a902af341d3b6d256491011a480c390a71ac0c7
rafalkaron <rafalkaron@gmail.com>
2022-06-25 20:27:25 +0200

    Hide navbar for short viewports

--- static/js/hide-navbar.js
+++ static/js/hide-navbar.js
@@ -1,6 +1,9 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  if (document.documentElement.clientWidth < 768) {
+  if (
+    document.documentElement.clientWidth < 768 ||
+    document.documentElement.clientHeight < 480
+  ) {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos || window.pageYOffset < 30) {
       document.getElementById("navbar").style.top = "0";
