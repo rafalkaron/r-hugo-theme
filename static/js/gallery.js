76b30e7f0e539406a572be36d0945fb99b7900aa
rafalkaron <rafalkaron@gmail.com>
2022-07-09 22:46:44 +0200

    Update modal logic and styling

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -4,32 +4,59 @@ let imgSrc;
 images.forEach((img) => {
   img.addEventListener("click", (e) => {
     imgSrc = e.target.src;
+    imgTitle = e.target.parentElement.querySelector("h6").innerText;
+    imgDesc = e.target.parentElement.querySelector(".description").innerText;
+    imgLocation = e.target.parentElement
+      .querySelector(".location")
+      .getAttribute("href");
     // Display the modal
-    imgModal(imgSrc);
+    imgModal(imgSrc, imgTitle, imgDesc, imgLocation);
   });
 });
 
 // Display the modal
-let imgModal = (src) => {
+let imgModal = (src, title, desc, location) => {
   const modal = document.querySelector(".modal");
   modal.style.display = "flex";
 
   // Add current image src
-  popupImage = document.querySelector("img.to-popup");
+  popupImage = document.querySelector(".to-popup");
   popupImage.setAttribute("src", src);
 
-  downloadIcon = document.querySelector("a.download.icon");
+  downloadIcon = document.querySelector(".modal-download");
   downloadIcon.setAttribute("href", src);
 
-  // Close on click
-  modal.onclick = () => {
+  modalTitle = document.querySelector(".modal-title");
+  modalTitle.textContent = title;
+
+  modalDesc = document.querySelector(".modal-description");
+  modalDesc.textContent = desc;
+
+  modalLocation = document.querySelector(".modal-location");
+  modalLocation.setAttribute("href", location);
+
+  modalClose = document.querySelector(".modal-close");
+
+  body = document.querySelector("body");
+  body.setAttribute("class", "modal-on");
+
+  // Close on close button click
+  modalClose.onclick = () => {
     modal.style.display = "none";
+    body.removeAttribute("class", "modal-on");
+  };
+
+  // Close on image click
+  popupImage.onclick = () => {
+    modal.style.display = "none";
+    body.removeAttribute("class", "modal-on");
   };
 
   // Close on ESC
   document.addEventListener("keydown", function (event) {
     if (event.key === "Escape") {
       modal.style.display = "none";
+      body.removeAttribute("class", "modal-on");
     }
   });
 };
