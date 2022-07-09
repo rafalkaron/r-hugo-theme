c24ab6daa5ad63e5db3ab3c6c9728b149c23f6ea
rafalkaron <rafalkaron@gmail.com>
2022-07-09 10:53:57 +0200

    Improve toc-indicator

--- static/js/toc-indicator.js
+++ static/js/toc-indicator.js
@@ -1,20 +1,25 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      const id = entry.target.querySelector("h2, h3").getAttribute("id");
-      if (entry.intersectionRatio > 0) {
-        document
-          .querySelector(`nav li a[href="#${id}"]`)
-          .parentElement.classList.add("active");
-      } else {
-        document
-          .querySelector(`nav li a[href="#${id}"]`)
-          .parentElement.classList.remove("active");
-      }
+if (document.documentElement.clientWidth >= 992) {
+  window.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const id = entry.target.querySelector("h2, h3").getAttribute("id");
+          if (entry.intersectionRatio > 0) {
+            document
+              .querySelector(`nav li a[href="#${id}"]`)
+              .parentElement.classList.add("active");
+          } else {
+            document
+              .querySelector(`nav li a[href="#${id}"]`)
+              .parentElement.classList.remove("active");
+          }
+        });
+      },
+      { rootMargin: "-25% 0px 0px 0px" }
+    );
+
+    document.querySelectorAll("section.article-section").forEach((section) => {
+      observer.observe(section);
     });
   });
-
-  document.querySelectorAll("section.article-section").forEach((section) => {
-    observer.observe(section);
-  });
-});
+}
