7ccc2836bb42fbff04c43e289aa642a0536d3fde
rafalkaron <rafalkaron@gmail.com>
2023-12-09 23:52:07 +0100

    Add event listener

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,8 +1,24 @@
+let lightbox = document.getElementById("lightbox");
+let lightboxImg = document.getElementById("lightbox-img");
+
 function openModal(src) {
-  document.getElementById("lightbox").style.display = "block";
-  document.getElementById("lightbox-img").setAttribute("src", src);
+  lightbox.style.display = "flex";
+  lightboxImg.setAttribute("src", src);
 }
 
 function closeModal() {
   document.getElementById("lightbox").style.display = "none";
 }
+
+document.addEventListener("keydown", (e) => {
+  e = e || window.event;
+  if (e.key === "Escape") {
+    {
+      if (lightbox.style.display !== "none") {
+        lightbox.style.display = "none";
+      } else {
+        lightbox.style.display = "none";
+      }
+    }
+  }
+});
