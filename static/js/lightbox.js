565b8142017231e7a114d2204b6f3418ba50b425
rafalkaron <rafalkaron@gmail.com>
2023-12-10 16:23:40 +0100

    Resolve body const conflict

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -3,19 +3,19 @@ const lightboxImg = document.getElementById("lightbox-img");
 const lightboxButtonDownload = document.getElementById(
   "lightbox-button-download"
 );
-const body = document.querySelector("body");
+const pageBody = document.querySelector("body");
 
 let lightboxImageSrc = null;
 
 function openModal(src) {
-  body.setAttribute("class", "modal-on");
+  pageBody.setAttribute("class", "modal-on");
   lightbox.style.display = "flex";
   lightboxImg.setAttribute("src", src);
   lightboxImageSrc = src;
 }
 
 function closeModal() {
-  body.removeAttribute("class", "modal-on");
+  pageBody.removeAttribute("class", "modal-on");
   document.getElementById("lightbox").style.display = "none";
 }
 
