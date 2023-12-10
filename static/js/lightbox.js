c4850a5201d1a0a9dca7470e22a4a761961ab414
rafalkaron <rafalkaron@gmail.com>
2023-12-10 09:48:17 +0100

    Add buttons

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,16 +1,26 @@
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
+const lightboxButtonDownload = document.getElementById(
+  "lightbox-button-download"
+);
 const body = document.querySelector("body");
+
+let lightboxImageSrc = null;
 
 function openModal(src) {
   body.setAttribute("class", "modal-on");
   lightbox.style.display = "flex";
   lightboxImg.setAttribute("src", src);
+  lightboxImageSrc = src;
 }
 
 function closeModal() {
   body.removeAttribute("class", "modal-on");
   document.getElementById("lightbox").style.display = "none";
+}
+
+function downloadFile() {
+  lightboxButtonDownload.setAttribute("href", lightboxImageSrc);
 }
 
 document.addEventListener("keydown", (e) => {
