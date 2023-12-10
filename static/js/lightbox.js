ef8b8ff4e9f208afc6b675f1580a3df8169db3ec
Rafał Karoń <rafalkaron@gmail.com>
2023-12-10 16:30:05 +0100

    Lightbox (#55)
    * Lightbox mvp
    
    * Improve lightbox script embed
    
    * Add event listener
    
    * Tweaks
    
    * Add buttons
    
    * Improve download button
    
    * Remove importants
    
    * Remove deprecated function
    
    * Lightbox adjustments
    
    * Rework button style
    
    * Scripts and dom loading
    
    * Fix colors
    
    * Resolve body const conflict
    
    * Remove async

---
+++ static/js/lightbox.js
@@ -0,0 +1,30 @@
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const lightboxButtonDownload = document.getElementById(
+  "lightbox-button-download"
+);
+const pageBody = document.querySelector("body");
+
+let lightboxImageSrc = null;
+
+function openModal(src) {
+  pageBody.setAttribute("class", "modal-on");
+  lightbox.style.display = "flex";
+  lightboxImg.setAttribute("src", src);
+  lightboxImageSrc = src;
+}
+
+function closeModal() {
+  pageBody.removeAttribute("class", "modal-on");
+  document.getElementById("lightbox").style.display = "none";
+}
+
+function downloadFile() {
+  lightboxButtonDownload.setAttribute("href", lightboxImageSrc);
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    lightbox.style.display = "none";
+  }
+});
