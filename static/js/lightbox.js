8350d04909b7fa43e42b12cce172e0846ff61cb2
rafalkaron <rafalkaron@gmail.com>
2023-12-16 23:56:43 +0100

    Add more info to lightbox

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -3,17 +3,28 @@ const lightboxImg = document.getElementById("lightbox-img");
 const lightboxButtonDownload = document.getElementById(
   "lightbox-button-download"
 );
+const lightboxFigcaption = document.getElementById("lightbox-figcaption");
 const pageBody = document.querySelector("body");
 
 let lightboxImageSrc = null;
 
-function openModal(src, alt) {
+function openModal(src, alt, title) {
   pageBody.setAttribute("class", "modal-on");
   lightbox.style.display = "flex";
   lightboxImg.setAttribute("src", src);
   if (alt != null) {
     lightboxImg.setAttribute("alt", alt);
+  } else {
+    lightboxImg.removeAttribute("alt");
   }
+  if (title != null && title != "") {
+    lightboxImg.setAttribute("title", title);
+    lightboxFigcaption.textContent = title;
+  } else {
+    lightboxImg.removeAttribute("title");
+    lightboxFigcaption.textContent = "";
+  }
+
   lightboxImageSrc = src;
 }
 
